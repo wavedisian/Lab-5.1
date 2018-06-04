1baabc348acbb40639443960f0cae00a34bae6ea
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-06-04 09:20:41 -0400

    etfw

---
+++ Lab 5.1/src/src/Node.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.*;
+
+public class Node {
+
+	private String name;
+	private ArrayList<Node> children;
+	
+	public void setChildren(ArrayList<Node> children)
+	{
+		this.children = children;
+	}
+	
+}
